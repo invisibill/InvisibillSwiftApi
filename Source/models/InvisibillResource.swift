59b73e9fd0e68f5822bbb62e09c9b6933e9fbc3b
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2016-07-26 09:47:34 -0400

    Parse :metadata and :identifier on resource model

--- Source/models/InvisibillResource.swift
+++ Source/models/InvisibillResource.swift
@@ -14,13 +14,17 @@ public class InvisibillResource: InvisibillModel {
     public var contract: InvisibillContract!
     public var timecards: Set<InvisibillTimecard>!
     public var invoices: Set<InvisibillInvoice>!
+    public var identifier: String!
     public var billRate: Double!
+    public var metadata: [String : AnyObject]!
 
     public override func mapping(map: Map) {
         id <- map["id"]
         contract <- map["contract"]
         timecards <- map["timecards"]
         invoices <- map["invoices"]
+        identifier <- map["identifier"]
         billRate <- map["bill_rate"]
+        metadata <- map["metadata"]
     }
 }
